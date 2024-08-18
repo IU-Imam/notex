85a36c5fb5251c1a2654f0ef45cd2301748cdc19
IU-Imam <imamuddin48@gmail.com>
2024-08-19 02:21:34 +0600

    created save method

--- notexapp/models.py
+++ notexapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils.text import slugify
+from django.utils.crypto import get_random_string
 # Create your models here.
 class Note(models.Model):
 
@@ -13,3 +14,18 @@ class Note(models.Model):
     category =models.CharField(max_length=16, choices=CATEGORY, default="PERSONAL")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True) 
+
+    def __str__(self) -> str:
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            #Generate initial slug
+            slug_base = slugify(self.title)
+            slug = slug_base
+
+            if Note.objects.filter(slug=slug).exists():
+                slug = f'{slug_base}-{get_random_string(5)}'
+            self.slug=slug
+
+        super(Note, self).save(*args, **kwargs)    
